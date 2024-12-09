4b156f563674e59ac86e07a48bd610fd31761b51
matei_malin <mateimalin08@gmail.com>
2024-12-09 14:50:29 +0200

    doamne ajuta

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include "header.hpp"
 
-
+using namespace std;
 int main() {
     
     
     functie a;
     a.input="cos(x)";
+    cout<< curatareInput(a.input);
     a.calculareOrdinePostfix();
     stackdump(a.sirPostfix);
     a.calcularePuncte(-2,5);
     for(int i=0;i<100;i++)
-        cout<<a.valori[i]<<" ";
+        cout<<a.valori[i].x<<" ";
 
     return 0;
 }
