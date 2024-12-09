c7493f186c431062e00b88aba1de59a2f5bf756c
matei_malin <mateimalin08@gmail.com>
2024-12-09 14:45:11 +0200

    implementare functie curatare

--- header.hpp
+++ header.hpp
@@ -51,14 +51,15 @@ public:
       input = "";
       delta = 0.01;
    }
-   friend float executareFunctie(float valoare, const functie& a);
-   void calcularePuncte(float start, float end){
-      float  valoare=start;
-      while(valoare<=end)
-      {  
-         float res=executareFunctie(valoare, *this);
+   friend float executareFunctie(float valoare, const functie &a);
+   void calcularePuncte(float start, float end)
+   {
+      float valoare = start;
+      while (valoare <= end)
+      {
+         float res = executareFunctie(valoare, *this);
          valori.emplace_back(res);
-         valoare+=delta;
+         valoare += delta;
       }
    }
 
@@ -70,9 +71,9 @@ public:
       // 2*3 + (8-3)/2
       for (i; i < lungime; i++)
       {
-         char c = input[i]; /// patru cazuri 1. Functii sin, cos, ln, tan / 2. numere sau variabila x, 3. operatori / 4. paranteze
-                            /// functia de prelucrare input se asigura ca nu vor exista probleme
-         if (c >= 'a' && c <= 'z' && c != 'x')// cazul 1, sin, cos , ln, tan, verificam daca este un cuvant care descrie o functie
+         char c = input[i];                    /// patru cazuri 1. Functii sin, cos, ln, tan / 2. numere sau variabila x, 3. operatori / 4. paranteze
+                                               /// functia de prelucrare input se asigura ca nu vor exista probleme
+         if (c >= 'a' && c <= 'z' && c != 'x') // cazul 1, sin, cos , ln, tan, verificam daca este un cuvant care descrie o functie
          {
             while (c >= 'a' && c <= 'z')
             {
@@ -81,10 +82,10 @@ public:
                c = input[i];
             }
             i--;
-            operatori.push(numar);// adaugam la final "numele" functiei in stiva de operatori
-            numar = ""; /// creeaza functie si o pune in stack
+            operatori.push(numar); // adaugam la final "numele" functiei in stiva de operatori
+            numar = "";            /// creeaza functie si o pune in stack
          }
-         else if (isdigit(c) || c == 'x')// daca avem x sau un numar 
+         else if (isdigit(c) || c == 'x') // daca avem x sau un numar
          {
             while (isdigit(c) || c == 'x')
             {
@@ -93,15 +94,15 @@ public:
                c = input[i];
             }
             i--;
-            sirPostfix.emplace_back(numar);// folosita pentru ca este mai rapida decat push_back
-            numar = ""; /// creeaza numar si il pune in sir
+            sirPostfix.emplace_back(numar); // folosita pentru ca este mai rapida decat push_back
+            numar = "";                     /// creeaza numar si il pune in sir
          }
          else if (isoperator(c))
          {
 
             while (operatori.empty() == 0 &&
                    (prioritateOperator(operatori.top()) > prioritateOperator(c) ||
-                    (prioritateOperator(operatori.top()) == prioritateOperator(c) && c != '^')))// verificam prioritatea operatorilor
+                    (prioritateOperator(operatori.top()) == prioritateOperator(c) && c != '^'))) // verificam prioritatea operatorilor
             {
                sirPostfix.emplace_back(operatori.top());
                operatori.pop();
@@ -112,7 +113,7 @@ public:
          }
          else if (c == '(')
             operatori.push("(");
-         else if (c == ')')// daca avem paranteza inchisa, dam pop la toti operatorii intalniti pana la '('
+         else if (c == ')') // daca avem paranteza inchisa, dam pop la toti operatorii intalniti pana la '('
          {
             while (operatori.empty() == 0 && operatori.top() != "(")
             {
@@ -120,9 +121,9 @@ public:
                sirPostfix.emplace_back(operatori.top());
                operatori.pop();
             }
-            if (operatori.empty() == 0)// elimina si paranteza '('
+            if (operatori.empty() == 0) // elimina si paranteza '('
                operatori.pop();
-            if (operatori.empty() == 0 && operatori.top()[0] >= 'a' && operatori.top()[0] <= 'z')// daca aveai in fata o functie de tipul cos, ln etc.
+            if (operatori.empty() == 0 && operatori.top()[0] >= 'a' && operatori.top()[0] <= 'z') // daca aveai in fata o functie de tipul cos, ln etc.
             {
                sirPostfix.emplace_back(operatori.top());
                operatori.pop();
@@ -152,7 +153,7 @@ float executareFunctie(float variabila, const functie &a)
          stiva.push(variabila);
       else if (isdigit(element[0]))
          stiva.push(stof(element));
-      else if (element.size() >= 2)// daca avem o functie, o inlocuim cu valoarea ei
+      else if (element.size() >= 2) // daca avem o functie, o inlocuim cu valoarea ei
       {
          float var = stiva.top();
          stiva.pop();
@@ -177,33 +178,117 @@ float executareFunctie(float variabila, const functie &a)
             stiva.push(var);
          }
       }
-      else// avem 2 variabile despartite printr-un operator
+      else // avem 2 variabile despartite printr-un operator
       {
-         float var1=stiva.top();
+         float var1 = stiva.top();
          stiva.pop();
-         float var2=stiva.top();
+         float var2 = stiva.top();
          stiva.pop();
-         if(element=="+")
-            stiva.push(var2+var1);
-         else if(element=="-")
-            stiva.push(var2-var1);
-         else if(element=="*")
-            stiva.push(var2*var1);
-         else if(element=="/")
-            stiva.push(var2/var1);
-         else 
+         if (element == "+")
+            stiva.push(var2 + var1);
+         else if (element == "-")
+            stiva.push(var2 - var1);
+         else if (element == "*")
+            stiva.push(var2 * var1);
+         else if (element == "/")
+            stiva.push(var2 / var1);
+         else
             stiva.push(pow(var2, var1));
-
       }
    }
    return stiva.top();
 }
 
-string curatareInput (string s)
+bool estefunctie(string s)
 {
-   stack<string> operanzi;
-   
+   return (s == "cos" || s == "sin" || s == "tan" || s == "ln");
 }
+bool isoperand(char c)
+{
+   return c=='*' || c=='-' ||c=='+' ||c=='^';
+}
+string curatareInput(string s)
+{
+   string cuvant;
+   string expresie;
+   int inparanteza = 0;
+   s.erase(remove(s.begin(), s.end(), ' '), s.end());
+   for (int i = 0; i < s.size(); i++)
+   {
+      char c = s[i];
+      cuvant = "";
+      if (c >= 'a' && c <= 'z' && c != 'x')
+      {
+         bool ok=1;
+         while (i < s.size() && c >= 'a' && c <= 'z' && c != 'x' && cuvant.size()<3 && ok) // daca ar trebui sa fie numele unei functii
+         {
+            cuvant += c;
+            
+            i++;
+            c = s[i];
+            if(cuvant=="ln")
+            ok=0;
+         }
+         i--;
 
+         if (estefunctie(cuvant)) // daca avem numele unei functii, o adaugam si deschidem o paranteza;
+         {
+            expresie += cuvant;
+            expresie += '(';
+            inparanteza++;
+         }
+         else
+         {
+            cout << "nu a fost introdus numele unei functii";
+            return 0;
+         }
+      }
+
+      else if (isdigit(c)) // daca e numar
+      {
+         cuvant += c;
+         c = s[i + 1];
+         i++;
+         while (isdigit(c)) // daca este numar format din mai multe cifre
+         {
+            cuvant += c;
+            i++;
+            c = s[i];
+         }
+         if (c == 'x') // daca dupa numar avem x, o sa traducem in nr*x
+            expresie += cuvant + "*" + 'x',i++;
+         else
+            expresie += cuvant;
+            i--;
+      }
+      else if (c == 'x')
+      {
+         if (isdigit(s[i + 1])) // daca dupa x avem un numar
+         {
+            i++;
+            c = s[i];
+            while (isdigit(c)) // daca este numar
+            {
+               cuvant += c;
+               i++;
+               c = s[i];
+            }
+            i--;
+            expresie += (cuvant + '*');
+         }
+         expresie += 'x';
+         expresie += ')';
+         inparanteza-- ;
+      }
+      else if(isoperand(c))
+         expresie += c;
+   }
+   while (inparanteza !=0)
+     { expresie += ')';
+       inparanteza--;
+     }
+
+   return expresie;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ int main() {
     
     
     functie a;
-    a.input="cos(x)";
+    a.input="cos 2^x+ sin 3x + ln  x 7 ln x";
+    cout<<curatareInput(a.input)<<' ';
     a.calculareOrdinePostfix();
     stackdump(a.sirPostfix);
     a.calcularePuncte(0,5);
     for(int i=0;i<100;i++)
         cout<<a.valori[i]<<" ";
 
+  
     return 0;
 }
