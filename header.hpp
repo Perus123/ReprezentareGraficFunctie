89c093f34a0edde6df91cfa93ec9d2e7c9bc85f2
Mogos Paul <smash_05@yahoo.com>
2024-12-09 11:38:39 +0200

    functie de calcul pentru valoarea x

--- header.hpp
+++ header.hpp
@@ -2,9 +2,9 @@
 #define HEADER_H
 #include <iostream>
 #include <string>
-#include <queue>
 #include <stack>
 #include <vector>
+#include <cmath>
 using namespace std;
 bool isoperator(char c)
 {
@@ -28,24 +28,21 @@ int prioritateOperator(const string &c)
       return 2;
    if (c[0] == '^')
       return 3;
-   if(c[0]>='a'&&c[0]<='z')
+   if (c[0] >= 'a' && c[0] <= 'z')
       return 4;
    return 0;
 }
-void stackdump(queue<string> st)
+void stackdump(vector<string> st)
 {
 
-   while (st.empty() == 0)
-   {
-      cout << st.front();
-      st.pop();
-   }
+   for (int i = 0; i < st.size(); i++)
+      cout << st[i] << " ";
 }
 class functie
 {
 public:
    string input;
-   queue<string> sirPostfix;
+   vector<string> sirPostfix;
    /// inputul trebuie prelucrat in REVERSE POLISH
    float delta;
    vector<float> valori;
@@ -58,13 +55,13 @@ public:
    void calculareOrdinePostfix()
    {
       int i = 0, lungime = input.size();
-      string numar = "";   ///string auxiliar pentru adaugat elemente in stiva
-      stack<string> operatori;   ///stiva de operatori
+      string numar = "";       /// string auxiliar pentru adaugat elemente in stiva
+      stack<string> operatori; /// stiva de operatori
       // 2*3 + (8-3)/2
       for (i; i < lungime; i++)
       {
-         char c = input[i];  ///patru cazuri 1. Functii sin, cos, ln, tan / 2. numere sau variabila x, 3. operatori / 4. paranteze
-                             ///functia de prelucrare input se asigura ca nu vor exista probleme
+         char c = input[i]; /// patru cazuri 1. Functii sin, cos, ln, tan / 2. numere sau variabila x, 3. operatori / 4. paranteze
+                            /// functia de prelucrare input se asigura ca nu vor exista probleme
          if (c >= 'a' && c <= 'z' && c != 'x')
          {
             while (c >= 'a' && c <= 'z')
@@ -75,7 +72,7 @@ public:
             }
             i--;
             operatori.push(numar);
-            numar = "";///creeaza functie si o pune in stack
+            numar = ""; /// creeaza functie si o pune in stack
          }
          else if (isdigit(c) || c == 'x')
          {
@@ -86,23 +83,22 @@ public:
                c = input[i];
             }
             i--;
-            sirPostfix.push(numar);
-            numar = ""; ///creeaza numar si il pune in sir
+            sirPostfix.emplace_back(numar);
+            numar = ""; /// creeaza numar si il pune in sir
          }
          else if (isoperator(c))
          {
 
-            while (operatori.empty() == 0 &&    
-                  (prioritateOperator(operatori.top()) > prioritateOperator(c) || 
-                  (prioritateOperator(operatori.top()) == prioritateOperator(c) && c != '^')))
+            while (operatori.empty() == 0 &&
+                   (prioritateOperator(operatori.top()) > prioritateOperator(c) ||
+                    (prioritateOperator(operatori.top()) == prioritateOperator(c) && c != '^')))
             {
-               sirPostfix.push(operatori.top());
+               sirPostfix.emplace_back(operatori.top());
                operatori.pop();
             }
             numar += c;
             operatori.push(numar);
-            numar = ""; ///creeaza operator si il pune in sir 
-
+            numar = ""; /// creeaza operator si il pune in sir
          }
          else if (c == '(')
             operatori.push("(");
@@ -111,21 +107,21 @@ public:
             while (operatori.empty() == 0 && operatori.top() != "(")
             {
 
-               sirPostfix.push(operatori.top());
+               sirPostfix.emplace_back(operatori.top());
                operatori.pop();
             }
             if (operatori.empty() == 0)
                operatori.pop();
-            if (operatori.empty()==0 && operatori.top()[0] >= 'a' && operatori.top()[0] <= 'z')
+            if (operatori.empty() == 0 && operatori.top()[0] >= 'a' && operatori.top()[0] <= 'z')
             {
-               sirPostfix.push(operatori.top());
+               sirPostfix.emplace_back(operatori.top());
                operatori.pop();
-            } ///rezolva parantezele
+            } /// rezolva parantezele
          }
       }
       while (operatori.empty() == 0)
       {
-         sirPostfix.push(operatori.top());
+         sirPostfix.emplace_back(operatori.top());
          operatori.pop();
       }
    }
@@ -134,5 +130,63 @@ public:
    /// cos(x)^2+x^3/5*2+sin(4*x/5)
    /// 2*3 + (8-3)/2
 };
+void executareFunctie(float variabila, const functie &a)
+{
+   stack<float> stiva;
+   int i = 0;
+   for (int i = 0; i < a.sirPostfix.size(); i++)
+   {
+      string element = a.sirPostfix[i];
+      if (element == "x")
+         stiva.push(variabila);
+      else if (isdigit(element[0]))
+         stiva.push(stof(element));
+      else if (element.size() >= 2)
+      {
+         float var = stiva.top();
+         stiva.pop();
+         if (element == "ln")
+         {
+            var = log(var);
+            stiva.push(var);
+         }
+         else if (element == "cos")
+         {
+            var = cos(var);
+            stiva.push(var);
+         }
+         else if (element == "sin")
+         {
+            var = tan(var);
+            stiva.push(var);
+         }
+         else if (element == "tan")
+         {
+            var = tan(var);
+            stiva.push(var);
+         }
+      }
+      else
+      {
+         float var1=stiva.top();
+         stiva.pop();
+         float var2=stiva.top();
+         stiva.pop();
+         if(element=="+")
+            stiva.push(var2+var1);
+         else if(element=="-")
+            stiva.push(var2-var1);
+         else if(element=="*")
+            stiva.push(var2*var1);
+         else if(element=="/")
+            stiva.push(var2/var1);
+         else 
+            stiva.push(pow(var2, var1));
+
+      }
+   }
+   cout<<"\n Rezultat: " <<stiva.top();
+}
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main() {
     
     
     functie a;
-    a.input="cos(x)^2+sin(x^3/4*5)*ln(e^x)";
+    a.input="cos(x)+3*x";
     a.calculareOrdinePostfix();
     stackdump(a.sirPostfix);
+    executareFunctie(1, a);
     return 0;
 }
